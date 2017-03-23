22fd1fd457b425c15809ffd16c2dbe4943bbfe79
JanethSanchezRiano <janeth_sanria@hotmail.com>
2017-03-23 12:27:06 -0500

    Crea la ruta POST /products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,15 @@ class ProductsController < ApplicationController
 
  def new
    @product = Product.new
+   @product = Product.new(products_params)
+   if @product.save
+     redirect_to products_path, notice: "El producto fue publicado con exito"
+  else
+    render :new
+  end
  end
+ private
+  def products_params
+    params.require(:product).permit(:name. :url, :description)
+  end
+end
